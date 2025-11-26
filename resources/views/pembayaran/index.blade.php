98ece166e2f327f5b3854ad85718d4be785c3e80
Salsa <salsaagustinanuriana@gmail.com>
2025-11-26 14:10:31 +0700

    inventaris barang sekolah

--- resources/views/pembayaran/index.blade.php
+++ resources/views/pembayaran/index.blade.php
@@ -5,7 +5,7 @@
     <div class="card shadow-sm border-0">
         <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
             <h5 class="mb-0">Data Pembayaran</h5>
-            <a href="{{ route('pembayaran.create') }}" class="btn btn-light btn-sm">+ Tambah Pembayaran</a>
+            <a href="{{ route('pembayaran.create') }}" class="btn btn-light btn-3">+ Tambah Pembayaran</a>
         </div>
 
         <div class="card-body">
